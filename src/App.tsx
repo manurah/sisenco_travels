50d733e3a096136edcd340fc7179c5da2e64a74a
manurah <manurahettiarachchi112233@gmail.com>
2025-08-20 09:26:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Packages from './pages/Packages';
-import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Packages from "./pages/Packages";
+import Gallery from "./pages/Gallery";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar always visible */}
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/packages" element={<Packages />} />
-          <Route path="/gallery" element={<Gallery/>}/>
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+
+        {/* Main content */}
+        <main className="flex-grow">
+          <Routes>
+            {/* 👇 Default route ("/") should load Home */}
+            <Route path="/" element={<Home />} />
+            <Route path="/packages" element={<Packages />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+
+        {/* Footer always visible */}
         <Footer />
       </div>
     </Router>
